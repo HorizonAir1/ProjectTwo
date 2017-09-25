4ab92fec9afc4b7a5f97bcb5f0545f5d7b34e72c
james kim <kim.jamesw@gmail.com>
2017-09-24 20:43:51 -0400

    created GetAllFlights method

--- Horizon.Logic/LogicFacade.cs
+++ Horizon.Logic/LogicFacade.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Horizon.Logic
 {
-    public class LogicFacade
+  public class LogicFacade
+  {
+    public List<List<string>> GetAllFlights(string startLoc, string destLoc, DateTime startSearch, DateTime endSearch)
     {
+      throw new NotImplementedException();
     }
+
+  }
 }
